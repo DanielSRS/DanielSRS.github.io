1550c8312fbfe54f85b1a5d744705e1bd02575ac
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-09-23 03:17:58 -0300

    ✨ Add Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { createStyledView } from "./components/StyledBuilder";
 
 export const App = () => {
   return (
     <AppContainer>
-      <Navbar />
-      {/*  */}
+      <Sections>
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Hero */}
+        <Hero />
+      </Sections>
     </AppContainer>
   )
 };
 
-const AppContainer = createStyledView({ flex: 1 });
+const AppContainer = createStyledView({
+  flex: 1,
+  alignItems: 'center',
+  backgroundColor: '#000',
+});
+
+const Sections = createStyledView({
+  maxWidth: 960,
+  flex: 1,
+  width: '100%',
+  backgroundColor: '#FFFFFF',
+});
